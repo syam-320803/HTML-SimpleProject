ff00ab9672e983afb5161f0fee5cf049e7fc69f1
syamsundara raju <rajusyamsundararaju9676@gmail.com>
2024-11-13 07:54:50 +0530

    first commit

---
+++ action_page.php
@@ -0,0 +1,6 @@
+<div class="form-check">
+    <label class="form-check-label">
+    <input type="radio" class="form-check-input" name="fav-language" id="fav-lang" value="checkedValue" checked>
+    Display value
+  </label>
+</div>
